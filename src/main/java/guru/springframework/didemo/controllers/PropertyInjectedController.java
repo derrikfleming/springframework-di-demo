ceaa71a350db9409e12b0a78ebf1dcd5e5d49f3d
derrikf2 <fleminde@mail.gvsu.edu>
2018-10-28 21:26:21 -0400

    add comment propertyinjection

--- src/main/java/guru/springframework/didemo/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/didemo/controllers/PropertyInjectedController.java
@@ -2,6 +2,9 @@ package guru.springframework.didemo.controllers;
 
 import guru.springframework.didemo.services.GreetingServiceImpl;
 
+/**
+ * Property injection least preferred method of DI
+ */
 public class PropertyInjectedController {
 
     public GreetingServiceImpl greetingService;
