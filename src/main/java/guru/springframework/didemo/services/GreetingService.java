3926111dbfbe949b4379c254980f8663c9e5cff8
derrikf2 <fleminde@mail.gvsu.edu>
2018-10-28 21:30:55 -0400

    greeting service

--- src/main/java/guru/springframework/didemo/services/GreetingService.java
+++ src/main/java/guru/springframework/didemo/services/GreetingService.java
@@ -1,4 +1,6 @@
 package guru.springframework.didemo.services;
 
 public interface GreetingService {
+
+    String sayGreeting();
 }
--- src/main/java/guru/springframework/didemo/services/GreetingServiceImpl.java
+++ src/main/java/guru/springframework/didemo/services/GreetingServiceImpl.java
@@ -1,4 +1,13 @@
 package guru.springframework.didemo.services;
 
-public class GreetingServiceImpl {
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingServiceImpl implements GreetingService {
+    public static final String HELLO_GURUS = "Hello Gurus!";
+
+    @Override
+    public String sayGreeting() {
+        return HELLO_GURUS;
+    }
 }
